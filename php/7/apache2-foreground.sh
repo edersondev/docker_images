ca09acfe0e993f9ef322728593acd69b0f23beb6
Ederson Ferreira <ederson.dev@gmail.com>
2017-08-22 11:20:46 -0300

    update foreground.sh

--- php/7/apache2-foreground.sh
+++ php/7/apache2-foreground.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-if [ ! -d "$APACHE_RUN_DIR" ]; then
-	mkdir "$APACHE_RUN_DIR"
-	chown $APACHE_RUN_USER:$APACHE_RUN_GROUP "$APACHE_RUN_DIR"
-fi
-if [ -f "$APACHE_PID_FILE" ]; then
-	rm "$APACHE_PID_FILE"
-fi
+set -e
+
+# Apache gets grumpy about PID files pre-existing
+rm -f /var/run/apache2/apache2.pid
+
 /usr/sbin/apache2ctl -D FOREGROUND
