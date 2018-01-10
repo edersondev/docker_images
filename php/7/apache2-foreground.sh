0acfc4079fdd21bf0cf9bf1e22d9330256bba591
Ederson Ferreira <ederson.dev@gmail.com>
2018-01-10 17:33:33 +0000

    update

--- php/7/apache2-foreground.sh
+++ php/7/apache2-foreground.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ -n "$WEB_DOCUMENT_ROOT" ]; then
+  exec gosu sed -i "s/html/html\/"$WEB_DOCUMENT_ROOT"/" /etc/apache2/sites-available/000-default.conf
+fi
+
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2.pid
 
